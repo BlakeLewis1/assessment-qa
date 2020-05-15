29198bd4a72ad408b78ff23ac641ddede15461ca
BlakeLewis1 <BLewis@academytrainee.com>
2020-05-15 09:28:02 +0100

    assessment

--- Code/import pytest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from Code import python1
-
-def test_one():
-    assert python1.one("hi","hello") == "hello"
-    assert python1.one("three", "two") == "three"
-    assert python1.one("three", "hello") == "three hello"
-    assert python1.
-    assert python1.one("fire","rib") == "fire"
